cfa26b207a52d64b8598fae41bf78dbc5eb049e0
thelearner411 <91204524+thelearner411@users.noreply.github.com>
2024-05-09 03:28:03 +0200

    initialise truffle project

---
+++ EtherTrove/contracts/EtherTrove.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract EthereumTrove {
+    address public owner;
+    uint256 public numCryptos;
+
+    constructor() {
+        /**/
+    }
+}
