16b8520f2173b3656a754315c7a68b26d3abcf56
thelearner411 <91204524+thelearner411@users.noreply.github.com>
2024-05-14 13:03:39 +0200

    Contract to create token and mint token

--- EtherTrove/contracts/EtherTrove.sol
+++ EtherTrove/contracts/EtherTrove.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract EthereumTrove {
-    address public owner;
-    uint256 public numCryptos;
+contract MyToken is ERC20, Ownable, ERC20Permit {
+    constructor(address initialOwner, string memory name, string memory symbol)
+        ERC20(name, symbol)
+        Ownable(initialOwner)
+        ERC20Permit(name)
+    {}
 
-    constructor() {
-        /**/
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
